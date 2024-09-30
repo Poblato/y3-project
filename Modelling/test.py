c46e232e4ba9c2588af935e0c4804df2d0484bc6
Blaise Robson <blaise@blaiserobson.com>
2024-09-30 13:37:11 +0100

    Bugfixes - basic MC sim functional

--- Modelling/test.py
+++ Modelling/test.py
@@ -1,37 +1,37 @@
 from enum import Enum
 import random as r
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Err(Enum):
     OK = 0
+    GenericNonFatalError = -1
     GenericFatalError = 1
-    GenericNonFatalError = 2
-    RangeError = 3
+    RangeError = 2
 
 def ShowError(err):
     errorCodes = {
-        "OK",
-        "Generic Non-Fatal Error",
-        "Generic Fatal Error",
-        "Range Error"
+        0: "OK",
+        -1: "Generic Non-Fatal Error",
+        1: "Generic Fatal Error",
+        2: "Range Error"
     }
     return errorCodes[err]
 
-xmin = 0
-xmax = 1
+xmin = 0.0
+xmax = 1.0
 numIterations = 1000
 
 def ProbDensityFunc(x):
     #Ensure Input is in range
-    if (x < xmin | x > xmax):
+    if ((x < xmin) | (x > xmax)):
         return {0, Err.RangeError}
     #Uniform
     return {1, Err.OK}
 
 def Model(x):
     #Ensure Input is in range
-    if (x < xmin | x > xmax):
+    if ((x < xmin) | (x > xmax)):
         return {0, Err.RangeError}
     
     # For now an exponential
@@ -40,21 +40,22 @@ def Model(x):
 # Set seed for debugging
 r.seed(2, 100)
 
-results = np.zeros(numIterations, 2)
+results = np.zeros((numIterations, 2))
 
 for i in range(numIterations):
     x = r.random()
     y, err = ProbDensityFunc(x)
-    if (err != Err.OK): # If error, skip rest of loop iteration
-        print(ShowError() + " in pdf, skipping iteration " + i)
-        continue
+    if (err != Err.OK):
+        print(ShowError(err) + " in pdf (iteration: " + i + ")")
+        if (err > 0): # Error is fatal, skip iteration
+            continue
     y, err = Model(x)
-    if (err != Err.OK): # If error, skip rest of loop iteration
-        print(ShowError() + " in model, skipping interation " + i)
-        ShowError()
-        continue
-    results[i] = {x, y}
+    if (err != Err.OK):
+        print(ShowError(err) + " in model (iteration: " + i + ")")
+        if (err > 0): # Error is fatal, skip iteration
+            continue
+    results[i] = np.array((x, y))
 
 fig, ax = plt.subplots()
-ax.scatter(results)
+ax.scatter(results[:,0], results[:,1])
 plt.show()
